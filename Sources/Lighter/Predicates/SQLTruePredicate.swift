6d0246f9a4ded5b1fa7d5a0fe7a997a6a079afac
Helge Heß <devteam@zeezide.de>
2024-04-07 15:03:02 +0200

    Make `Bool` conform to `SQLPredicate`
    Should allow writing predicate queries like
    this:
    
       `db.delete(from: \.bodies) { _ in true }`
    
    Also added a BoolPredicate to back that (for
    some reason only had a TruePredicate before?)

--- Sources/Lighter/Predicates/SQLTruePredicate.swift
+++ Sources/Lighter/Predicates/SQLTruePredicate.swift
@@ -19,3 +19,40 @@ public struct SQLTruePredicate: SQLPredicate {
     builder.append(Self.sql)
   }
 }
+
+extension SQLPredicate where Self == SQLTruePredicate {
+  
+  static var `true` : SQLTruePredicate { .shared }
+}
+
+/**
+ * Returns a predicate that always matches or never.
+ */
+public struct SQLBoolPredicate: SQLPredicate {
+
+  public static let `true`  = Self(value: true)
+  public static let `false` = Self(value: false)
+  
+  public let value : Bool
+  
+  public func generateSQL<Base>(into builder: inout SQLBuilder<Base>) {
+    builder.append(value ? "1 = 1" : "1 = 0")
+  }
+}
+
+extension SQLPredicate where Self == SQLBoolPredicate {
+  
+  static var `false` : SQLBoolPredicate { .false }
+}
+
+extension Bool: SQLPredicate {
+
+  public func generateSQL<Base>(into builder: inout SQLBuilder<Base>) {
+    if self {
+      SQLTruePredicate.shared.generateSQL(into: &builder)
+    }
+    else {
+      SQLBoolPredicate.false.generateSQL(into: &builder)
+    }
+  }
+}
